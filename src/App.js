f43feb1ab4e35199543047a0636863c44fd8dff1
clocktower39 <clocktower39@aol.com>
2021-05-24 23:29:19 -0700

    Merchandise component

--- src/App.js
+++ src/App.js
@@ -1,109 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  Badge,
-  Button,
-  Card,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  CardActions,
-  Container,
-  Drawer,
-  Typography,
-  makeStyles,
-  IconButton,
-  Slide,
-} from "@material-ui/core";
-import {
-  ShoppingCart,
-  AddShoppingCart,
-} from "@material-ui/icons";
-import { addToCart } from "./Redux/actions";
+import React, { useState } from "react";
+import { Drawer } from "@material-ui/core";
 import Cart from './Components/Cart';
+import Merchandise from './Components/Merchandise';
 import "./App.css";
 
-const useStyles = makeStyles({
-  IconButton: {
-    margin: "25px 0",
-  },
-  Card: {
-    margin: "25px 0",
-  },
-  CartItemsContainer: {
-    padding: "0 0 50px 0",
-  },
-});
-
 function App() {
-  const merchandise = useSelector((state) => state.merchandise);
-  const cart = useSelector((state) => state.cart);
-  const [slideIn, setSlideIn] = useState(false);
-  const classes = useStyles();
-  const dispatch = useDispatch();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const toggleDrawer = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
 
-  useEffect(() => {
-    setSlideIn(true);
-  }, []);
-
   return (
     <div className="App">
-      <Container maxWidth="sm">
-        <IconButton className={classes.IconButton} onClick={toggleDrawer}>
-          <Badge
-            badgeContent={cart.length}
-            color="primary"
-            className={classes.CartBadge}
-          >
-            <ShoppingCart style={{ color: "white" }} />
-          </Badge>
-        </IconButton>
-
-        {merchandise.map((item, index) => {
-          return (
-            <Slide direction="right" mountOnEnter unmountOnExit in={slideIn}>
-              <Card key={index} className={classes.Card}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    alt={item.description}
-                    height="140"
-                    image="#"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {item.title}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="textSecondary"
-                      component="p"
-                    >
-                      ${item.price}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-                <CardActions>
-                  <Button
-                    size="small"
-                    color="primary"
-                    onClick={() => dispatch(addToCart(item))}
-                  >
-                    <AddShoppingCart />
-                  </Button>
-                </CardActions>
-              </Card>
-            </Slide>
-          );
-        })}
-      </Container>
+      <Merchandise toggleDrawer={()=>{toggleDrawer()}} />
       <Drawer anchor="bottom" variant="temporary" open={isDrawerOpen}>
-        <Cart open={isDrawerOpen} toggleDrawer={()=>{toggleDrawer()}}/>
+        <Cart toggleDrawer={()=>{toggleDrawer()}}/>
       </Drawer>
     </div>
   );
